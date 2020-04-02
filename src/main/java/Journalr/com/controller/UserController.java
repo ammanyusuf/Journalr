7770c83efe5788c8af0217557b66d31fa4fcee59
ammanyusuf <amman.yusuf@ucalgary.ca>
2020-04-01 19:11:44 -0600

    Revert "Removed some useless code"
    This reverts commit 4f781dfcfec99fb04b5914084ee31050df037aea.

--- src/main/java/Journalr/com/controller/UserController.java
+++ src/main/java/Journalr/com/controller/UserController.java
@@ -12,6 +12,7 @@ import Journalr.com.repositories.UserRepository;
 import java.util.*;
 //import java.util.Map;
 
+import Journalr.com.model.Author;
 import Journalr.com.model.User;
 
 //@RestController
@@ -66,6 +67,7 @@ public class UserController {
             return "redirect:/admin";
     }
 
+
     /**
      * This method parses the unique user id that was passed through the url.  It then retrieves
      * the user with that id and sets up the edituser template for the model to go to.  This is all
@@ -96,4 +98,120 @@ public class UserController {
         return "redirect:/admin";
     }
 
+    /*
+    Method that can update an existing user and/or create one if it doesn't exist
+    */
+    @RequestMapping(method = RequestMethod.POST, path="/update/{id}")
+    public @ResponseBody String updateUser (@PathVariable String id, @RequestParam String userName, 
+    @RequestParam String firstName, @RequestParam String lastName,
+    @RequestParam String password, @RequestParam String email, 
+    @RequestParam Boolean active) {
+            int UID = Integer.parseInt(id);
+            Optional<User> optional = userRepository.findById(UID);                 // Need to since Optional<User> type can possibly be null
+            
+            if(optional.isPresent()) {
+                // value is present inside Optional
+                User aUser = optional.get();
+                aUser.setUserName(userName);
+                aUser.setFirstName(firstName);
+                aUser.setLastName(lastName);
+                aUser.setPassword(password);
+                aUser.setEmail(email);
+                aUser.setActive(active);
+                userRepository.save(aUser);
+                return "User: " + id + " updated";
+            } else {
+                // user does not exist, so we create one
+                // User defaults to AUTHOR
+                User aUser = new User();
+                aUser.setUserName(userName);
+                aUser.setFirstName(firstName);
+                aUser.setLastName(lastName);
+                aUser.setPassword(password);
+                aUser.setEmail(email);
+                aUser.setActive(active);
+                aUser.setRoles("ROLE_AUTHOR");
+                userRepository.save(aUser);
+                return "User Created";
+            }	
+    }
+
+    /*
+    Method that can update an existing user's password
+    */
+    @RequestMapping(method = RequestMethod.POST, path="/update/password/{id}")
+    public @ResponseBody String updateUserPassword (@PathVariable String id, @RequestParam String password) {
+            int UID = Integer.parseInt(id);
+            Optional<User> optional = userRepository.findById(UID);                 // Need to since Optional<User> type can possibly be null
+            
+            if(optional.isPresent()) {
+                // value is present inside Optional
+                User aUser = optional.get();
+                aUser.setPassword(password);
+                userRepository.save(aUser);
+                return "User: " + id + " password updated";
+            } else {
+                return "User does not exist";
+            }	
+    }
+
+    /*
+    Method that can update an existing user's username
+    */
+    @RequestMapping(method = RequestMethod.POST, path="/update/username/{id}")
+    public @ResponseBody String updateUserName (@PathVariable String id, @RequestParam String userName) {
+            int UID = Integer.parseInt(id);
+            Optional<User> optional = userRepository.findById(UID);                 // Need to since Optional<User> type can possibly be null
+            
+            if(optional.isPresent()) {
+                // value is present inside Optional
+                User aUser = optional.get();
+                aUser.setUserName(userName);
+                userRepository.save(aUser);
+                return "User: " + id + " username updated to " + userName;
+            } else {
+                return "User does not exist";
+            }	
+    }
+
+    /*
+    Method to show all users in the User table
+    */
+    @RequestMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+      // This returns a JSON or XML with the users
+      return userRepository.findAll();
+    }
+
+    /*
+    Method to show a given user by their first name
+    */
+    @RequestMapping(path="/user/firstname/{firstName}", method = RequestMethod.GET) 
+    public @ResponseBody List<User> getUserByFirstName(@PathVariable("firstName") String firstName) {
+        return userRepository.findByFirstNameContaining(firstName);
+
+    }
+
+    /*
+    Method to show a given user by their last name
+    */
+    @RequestMapping(path="/user/lastname/{lastName}", method = RequestMethod.GET) 
+    public @ResponseBody List<User> getUserByLastName(@PathVariable("lastName") String lastName) {
+        return userRepository.findByLastNameContaining(lastName);
+
+    }
+
+/*
+    @RequestMapping(method = RequestMethod.POST, path="/search")
+    public List<User> search(@RequestBody String first) {
+      return userRepository.findAll();
+    }
+*/
+/*
+    @RequestMapping(method = RequestMethod.GET, value = "/users")
+    @ResponseBody
+    public List<User> search(@RequestParam(value = "search") String body){
+        String searchTerm = body;
+        return userRepository.findByFirstName(searchTerm);
+    }*/
 }
