18fc93cbe22f1a4bbf52535261bb923e707131c7
isaguimet <38235844+isaguimet@users.noreply.github.com>
2020-04-04 00:00:41 -0600

    Add query to find papers by topic

--- src/main/java/Journalr/com/repositories/PaperRepository.java
+++ src/main/java/Journalr/com/repositories/PaperRepository.java
@@ -16,4 +16,8 @@ public interface PaperRepository extends JpaRepository<Paper, Integer> {
     @Query(value = "SELECT reviewer_id FROM review_paper,paper WHERE review_paper.paper_id = ?1 AND review_paper.paper_id=paper.paperid",
             nativeQuery = true)
     List<Integer> findReviewersPerPaper(int paper_id);
+
+    @Query(value = "SELECT * FROM paper WHERE paper.topic = ?1",
+        nativeQuery = true)
+    List<Paper> findPapersByTopic(String topic);
 }
