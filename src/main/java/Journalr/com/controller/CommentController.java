f865d451b54a2ffed8f300ef00a821a6165e1c57
davidmaca248 <david.macababayao@ucalgary.ca>
2020-04-09 16:03:50 -0600

    completed reviewer addComment on paper

--- src/main/java/Journalr/com/controller/CommentController.java
+++ src/main/java/Journalr/com/controller/CommentController.java
@@ -1,7 +1,9 @@
 package Journalr.com.controller;
 
 import java.sql.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -11,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,47 +21,58 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import Journalr.com.model.Comment;
+import Journalr.com.model.Paper;
 import Journalr.com.model.Reviewer;
 import Journalr.com.model.User;
 import Journalr.com.model.UserDetailsClass;
 import Journalr.com.repositories.CommentRepository;
+import Journalr.com.repositories.PaperRepository;
+import Journalr.com.repositories.ReviewerRepository;
 
 @Controller
 public class CommentController {
 
     @Autowired
     CommentRepository commentRepository;
-
-    // post method from the submit comment page
-		// adds the comment into the comment table in database with the attribut
+    
+    @Autowired
+    PaperRepository paperRepository;
+    
+    @Autowired
+    ReviewerRepository reviewerRepository;
 	
-	@RequestMapping(value="/comment", method=RequestMethod.GET)
-	public String getComment() {
-		return "comment";              // get method to acces the comment page
+	@RequestMapping(path="/addComment/{paperId}")
+    public String showCommentPage (Model model, @PathVariable(name = "paperId") int paperId) {
+		
+    	model.addAttribute("paperId", paperId);
+    	
+		return "addComment";
 	}
 
 	// method that adds the comment to the database
-	@RequestMapping(value="/comment", method = RequestMethod.POST)//, path="/comment")
-	public String addComment (@ModelAttribute(name="comment") String comment, Model model) {
+    @RequestMapping(path="/addComment/{paperId}", method = RequestMethod.POST)
+    public String saveDeadline (@PathVariable(name = "paperId") int paperId, @ModelAttribute(name="comment") String comment) throws ParseException {
 		 
 		Comment commentObj = new Comment();
-			
-		//SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
-		Date date = new Date(System.currentTimeMillis());     // this is to get the current date
-		//formatter.format(date)
 
-		UserDetailsClass userDetails = (UserDetailsClass) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		//Get the current date
+		Date date = new Date(System.currentTimeMillis());     
+
+		//Get the current user
+		UserDetailsClass userDetails = (UserDetailsClass) SecurityContextHolder.getContext().getAuthentication().getPrincipal(); 
 		
-			
-		Reviewer reviewer = new Reviewer();            // to be passed to the comment object
-		reviewer.setReviewerId(userDetails.getId());   //sets the Id of the current user into the reviewer object
-			
-		//find the reviewer in the database using the Id (reviewerId) then get that reviewer (since our reviewer only has the Id field initialized)
-			
-	    commentObj.setComment(comment);             // this is entered by the user (reviewer)
-	    commentObj.setReviewer(reviewer);          // this should get the reviewerId
-	    //commentObj.setPaperId(paperId);                // this should get the paperId
-	    commentObj.setCommentDate(date);        // this should get the current date
+		//Put current user into reviewer object
+		int id = userDetails.getId();
+		Reviewer reviewer = reviewerRepository.findById(id).get();         // to be passed to the comment object
+		
+		//Find the paper in the database using given paperId
+		Paper paper = paperRepository.findById(paperId).get();
+		
+		//Save comment
+	    commentObj.setComment(comment);
+	    commentObj.setReviewer(reviewer);
+	    commentObj.setPaper(paper);
+	    commentObj.setCommentDate(date);
 	    commentRepository.save(commentObj);
 	            
 	    return "paperTopics";
