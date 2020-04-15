272c742f77b525debbd28dce0f3f7fd5f46f2876
ammanyusuf <amman.yusuf@ucalgary.ca>
2020-04-14 18:37:11 -0600

    Added even more comments

--- src/main/java/Journalr/com/model/Reviewer.java
+++ src/main/java/Journalr/com/model/Reviewer.java
@@ -24,56 +24,129 @@ public class Reviewer extends User{
     @OneToMany(mappedBy = "reviewer")
 	private Set<Comment> comments = new HashSet<>();
 
-    public Reviewer() {}
+    /**
+     * The basic constructor for the reviewer
+     */
+    public Reviewer() {
 
+    }
+
+    /**
+	 * A basic constructor of the reviewer class.  It calls the super constructor for the user class
+	 * @param userName The username of the reviewer
+	 * @param firstName The firstname of the reviewer
+	 * @param lastName The last name of the reviewer
+	 * @param email The email of the reviewer
+	 * @param password The password of the reviewer
+	 */
 	public Reviewer(String userName, String firstName, String lastName, String email, String password) {
 		super(userName, firstName, lastName, email, password);
     }
 
+    /**
+	 * A constructor that creates a reviewer given a user input
+	 * Calls the super constructor.
+	 * @param user The user that we wish to assign as an reviewer to
+	 */
     public Reviewer(User user) {
         super(user);
     }
     
+    //Setters and Getters
+
+    /**
+     * This method sets the reviewer id of the reviewer
+     * @param Id teh reviewer id
+     */
     public void setReviewerId(int Id) {
     	super.setUserId(Id);
     }
     
+    /**
+     * This method gets the affiliation of the reviewer
+     * @return The affiliation
+     */
     public String getAffiliation() {
         return affiliation;
     }
 
+    /**
+     * This method sets the affiliation of the reviewer
+     * @param affiliation The affiliation of the reviewer
+     */
     public void setAffiliation(String affiliation) {
         this.affiliation = affiliation;
     }
 
+    
+    /**
+	 * This method retrieves the instance of papers of that reviewer.
+	 * Not encapsulating since it allows access to the instances of papers
+	 * directly in the database
+	 * @return a set of papers
+	 */
     public Set<Paper> getPapers() {
-        return papers;                  // Not an encapsulating method for now
+        return papers;                  
     }
 
+    /**
+	 * This method sets the instance of papers of that reviewer.
+	 * Not encapsulating since it allows access to the instances of papers
+	 * directly in the database
+	 * @param papers the set of papers we wish to add to the reviewer
+	 */
     public void setPapers(Set<Paper> papers) {
         this.papers = papers;
     }
 
+    /**
+	 * This method directly adds a paper to the reviewer's set
+	 * @param paper The paper we wish to add
+	 */
     public void addPapers(Paper paper) {
         this.papers.add(paper);
     }
 
+    /**
+	 * This method retrieves the instance of comments of that reviewer.
+	 * Not encapsulating since it allows access to the instances of comments
+	 * directly in the database
+	 * @return a set of comments
+	 */
     public Set<Comment> getComments() {
         return comments;                  // Not an encapsulating method for now
     }
 
+    /**
+	 * This method sets the instance of comments of that reviewer.
+	 * Not encapsulating, since it allows access to the instances of comments
+	 * directly in the database
+	 * @param comments the set of comments we wish to add to the reviewer
+	 */
     public void setComments(Set<Comment> comments) {
         this.comments = comments;
     }
 
+    /**
+	 * This method directly adds a comment to the reviewer's set
+	 * @param comment The coment we wish to add
+	 */
     public void addComment(Comment comment) {
         this.comments.add(comment);
     }
 
+    /**
+     * This method gets the favourtie topic of the reviewer
+     * @return The favourite topic of the reviewer
+     */
     public String getFavouriteTopic() {
         return favouriteTopic;
     }
 
+    /**
+     * This method sets the favourtie topic of the reviewer
+     * @param topic the topic
+     */
     public void setFavouriteTopic(String topic) {
         this.favouriteTopic = topic;
     }
--- src/main/java/Journalr/com/model/User.java
+++ src/main/java/Journalr/com/model/User.java
@@ -22,7 +22,7 @@ public class User {
 	@Column(name = "is_active")
 	private boolean active;
 	
-	@Column(name = "role_type")			// Might need to change this to roles instead
+	@Column(name = "role_type")			
 	private String roles;
 
 	@Column(name = "first_name")
@@ -31,11 +31,21 @@ public class User {
 	@Column(name = "last_name")
 	private String lastName;
 
-	
+	/**
+	 * The basic constructor of the user
+	 */
 	public User() {
 		
 	}
 	
+	/**
+	 * A basic constructor of the user class.
+	 * @param userName The username of the user
+	 * @param firstName The firstname of the user
+	 * @param lastName The last name of the user
+	 * @param email The email of the user
+	 * @param password The password of the user
+	 */
 	public User(String userName, String firstName, String lastName, String email, String password) {
 		this.userName = userName;
 		this.firstName = firstName;
@@ -44,6 +54,11 @@ public class User {
 		this.password = password;
 	}
 
+	/**
+	 * A copy constructor that creates a new User by copying the values of 
+	 * the passed in user
+	 * @param aUser the user we wish to copy
+	 */
 	public User(User aUser) {
 		this.userId = aUser.getUserId();
 		this.userName = aUser.getUserName();
@@ -55,77 +70,148 @@ public class User {
 		this.lastName = aUser.getLastName();
 	}
 
+	// Setters and getters
 
+	/**
+	 * This method gets the user id of the user
+	 * @return the user id
+	 */
 	public int getUserId() {
 		return userId;
 	}
 	
+	/**
+	 * This method sets the user id of the user
+	 * @param userId the user id of the user
+	 */
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
 	
+	/**
+	 * This method gets the username of the user
+	 * @return the username
+	 */
 	public String getUserName() {
 		return userName;
 	}
 	
+	/**
+	 * This method sets the username of the user
+	 * @param userName the username
+	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 	
+	/**
+	 * This method gets the password of the user
+	 * @return the password
+	 */
 	public String getPassword() {
 		return password;
 	}
 	
+	/**
+	 * This method sets the password of the user
+	 * @param password the password
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
+	/**
+	 * This method gets the first name of the user
+	 * @return the first name
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 	
+	/**
+	 * This method sets the first name of the user
+	 * @param name the first name
+	 */
 	public void setFirstName(String name) {
 		this.firstName = name;
 	}
 
+	/**
+	 * This method gets the last name of the user
+	 * @return the last name
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 	
+	/**
+	 * This method sets the last name of the user
+	 * @param name The last name
+	 */
 	public void setLastName(String name) {
 		this.lastName = name;
 	}
 
+	/**
+	 * This method gets the email of the user
+	 * @return the email
+	 */
 	public String getEmail() {
 		return email;
 	}
 	
+	/**
+	 * This method sets the email of the user
+	 * @param email the email
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * This method gets the active attribute of the user
+	 * @return the active attribute, true or false
+	 */
 	public boolean isActive() {
         return active;
     }
 
+	/**
+	 * This method sets the active attribute
+	 * @param active the active attribute of the user, either true or false
+	 */
     public void setActive(boolean active) {
         this.active = active;
     }
 
+	/**
+	 * This method gets the role of the user
+	 * @return The role of the user
+	 */
     public String getRoles() {
         return roles;
     }
 
+	/**
+	 * This method sets the role of the user
+	 * @param roles The role
+	 */
     public void setRoles(String roles) {
         this.roles = roles;
     }
 
+	/**
+	 * This method is a basic to string method for the user
+	 */
 	@Override
 	public String toString() {
 		return "User{" + "id= " + userId + ", " + "Name= " + firstName + " " + lastName + ", " + "Email= " + email + "}";
 	}
 
-
+	/**
+	 * This method copies the values of a passed in user
+	 * @param user The user we want to copy values into
+	 */
 	public void copyValues(User user) {
         this.setUserName(user.getUserName());
         this.setPassword(user.getPassword());
